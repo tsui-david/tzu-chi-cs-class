9f32fbf99dfbbce285245afaa41798bf9bf5748f
david <david@prime.lan>
2021-02-07 09:29:48 -0500

    test

---
+++ class12-array-drills/checks.py
@@ -0,0 +1,54 @@
+import unittest
+from drills import *
+
+class CheckDrills(unittest.TestCase):
+    def test_ex1(self):
+        ans = ex1([1,2,3,4,5])
+        self.assertEqual(ans, 5)
+    
+    def test_ex2(self):
+        ans = ex2([1,2,3])
+        self.assertEqual(ans, 1)
+    
+    def test_ex3(self):
+        ans = ex3([1,2,3,4,5,6,7,8,9,10])
+        self.assertEqual(ans, 10)
+    
+    def test_ex4(self):
+        ans = ex4()
+        self.assertEqual(ans, ['a','a','a','a','a'])
+    
+    def test_ex5(self):
+        ans = ex5()
+        compare = ['a'*100]
+        self.assertEqual(ans, compare)
+    
+    def test_ex6(self):
+        ans = ex6(6)
+        self.assertEqual(ans, ['a','a','a','a','a','a'])
+    
+    def test_ex7(self):
+        ans = ex7([1,2,3])
+        self.assertEqual(ans, [1,2])
+
+    def test_ex8(self):
+        ans = ex7([1,2,3])
+        self.assertEqual(ans, [1])
+
+    def test_ex9(self):
+        ans = ex7([1,2,3],3)
+        self.assertEqual(ans, [1])
+
+    def test_ex10(self):
+        test = [1,2,3]
+        ans = ex10(test)
+        self.assertIsNot(test, ans)
+        self.assertEqual(ans, [1,2,3])
+
+    def test_ex11(self):
+        test = [1,2,3]
+        self.assertEqual(ans, [1,2,3,1,2,3,1,2,3])  
+
+if __name__.__contains__("__main__"):
+    print(__doc__)
+    unittest.main()
---
+++ class12-array-drills/drills.py
@@ -0,0 +1,78 @@
+"""
+- The following are a list of exercises to get you familiar with loops, arrays, and variables.
+- You might or might not need to modify the arguments to the function!!
+- Please adjust base on the comment of the exercise.
+- After you complete the exercsise, uncomment the assertion to check your answer
+"""
+
+def ex1():
+    """
+    given an array return the 5th element in the array
+    """
+    return
+
+def ex2():
+    """
+    given an array return the first element in the array
+    """
+    return
+
+def ex3():
+    """
+    given an array return the last element in the array
+    """
+    return
+
+def ex4():
+    """
+    create an array with 5 character "a" inside
+    """
+    return
+
+def ex5():
+    """
+    create an array with 100 character "a" inside
+    """
+    return
+
+def ex6():
+    """
+    given an input n, create an array with n character "a" inside
+    """
+    return
+
+def ex7():
+    """
+    given an array, remove the last element
+    if array is empty return empty
+    """
+    return
+
+def ex8():
+    """
+    given an array, remove all BUT the first element
+    if array is empty return empty
+    [1,2,3] -> [1]
+    """
+    return
+
+def ex9():
+    """
+    given an array, and n, remove n elements from the array
+    if array is empty return empty
+    """
+    return
+
+def ex10():
+    """
+    given an array, return a new array that is a copy of the array
+    """
+    return
+
+def ex11():
+    """
+    given an array, return an array that copies the input array 3 times.
+    ex: [1] -> [1,1,1]
+        [2,2] -> [2,2,2,2,2,2]
+    """
+    return
